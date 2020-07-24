83875e6880b64db9d2147a4eae02167386644df3
b.swierzewski <b.swierzewski@7technology.pl>
2020-07-24 12:29:34 +0200

    Add fontawesome to categories

--- src/app/dashboard/categories/categories.component.ts
+++ src/app/dashboard/categories/categories.component.ts
@@ -7,13 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
   categories = [
-    { Id: 1, Name: 'Sensor' },
-    { Id: 2, Name: 'Output' },
-    { Id: 3, Name: 'Light' },
-    { Id: 4, Name: 'Switch' },
-    { Id: 5, Name: 'Fan' },
-    { Id: 6, Name: 'Display' },
-    { Id: 7, Name: 'Misc' },
+    { Id: 1, Name: 'Sensor', FontAwesome: 'fas fa-signal' },
+    { Id: 2, Name: 'Output', FontAwesome: 'fas fa-sign-out-alt' },
+    { Id: 3, Name: 'Light', FontAwesome: 'fas fa-lightbulb' },
+    { Id: 4, Name: 'Switch', FontAwesome: 'fas fa-random' },
+    { Id: 5, Name: 'Fan', FontAwesome: 'fas fa-fan' },
+    { Id: 6, Name: 'Display', FontAwesome: 'fas fa-desktop' },
+    { Id: 7, Name: 'Misc', FontAwesome: 'fas fa-globe' },
   ];
 
   constructor() {}
