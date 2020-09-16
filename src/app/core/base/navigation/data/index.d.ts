8117e7e1f580519d551dc9509cd901aa23000122
b.swierzewski <swierzewski.bartosz@gmail.com>
2020-09-16 22:00:02 +0200

    Rename file index.d.ts to index.ts

--- src/app/core/base/navigation/data/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { NavMenuItem } from './NavMenuItem';
-export { NavbarData } from './NavbarData';
-export { SideMenuItem } from './SideMenuItem';
-export { SidebarData } from './SidebarData';
---
+++ src/app/core/base/navigation/data/index.ts
@@ -0,0 +1,4 @@
+export * from './NavMenuItem';
+export * from './NavbarData';
+export * from './SideMenuItem';
+export * from './SidebarData';
