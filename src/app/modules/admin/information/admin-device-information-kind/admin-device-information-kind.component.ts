6cb8cd15595692bdaf743187ac559c015993806f
b.swierzewski <swierzewski.bartosz@gmail.com>
2020-09-21 18:50:36 +0200

    Refactoring kind

--- src/app/modules/admin/information/admin-device-information-kind/admin-device-information-kind.component.ts
+++ src/app/modules/admin/information/admin-device-information-kind/admin-device-information-kind.component.ts
@@ -27,18 +27,20 @@ export class AdminDeviceInformationKindComponent implements OnInit, OnDestroy {
     this.loadKinds();
   }
 
-  ngOnInit(): void {
-    this.tabListFormService.selectionChangeSubject$.pipe(takeWhile(() => this.isAlive)).subscribe((data) => {
-      this.selectionChange(data);
-    });
-
-    this.tabListFormService.removeSubject$.pipe(takeWhile(() => this.isAlive)).subscribe((data) => {
-      this.removeClick(data);
-    });
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.isAlive = false;
+  }
+
+  selectionChangeSubscribe(): void {
+    this.tabListFormService.selectionChangeSubject$.pipe(takeWhile(() => this.isAlive)).subscribe((data) => {
+      if (data.name) {
+        this.model.id = data.id;
+        this.model.icon = data.icon;
+        this.model.name = data.name;
+      }
+    });
   }
 
   loadKinds(): void {
@@ -46,26 +48,20 @@ export class AdminDeviceInformationKindComponent implements OnInit, OnDestroy {
   }
 
   // Method to subscribe subject
-  removeClick(data: Kind): void {
-    this.alertService.confirm('Are you sure?', () => {
-      this.kindInformationService.removeKind(data.id).subscribe(
-        (next) => {
-          this.onClearClick();
-          this.loadKinds();
-        },
-        (error) => {
-          this.alertService.error(error);
-        }
-      );
+  removeSubscribe(): void {
+    this.tabListFormService.removeSubject$.pipe(takeWhile(() => this.isAlive)).subscribe((data) => {
+      this.alertService.confirm('Are you sure?', () => {
+        this.kindInformationService.removeKind(data.id).subscribe(
+          (next) => {
+            this.onClearClick();
+            this.loadKinds();
+          },
+          (error) => {
+            this.alertService.error(error);
+          }
+        );
+      });
     });
-  }
-
-  selectionChange(data: Kind): void {
-    if (data.name) {
-      this.model.id = data.id;
-      this.model.icon = data.icon;
-      this.model.name = data.name;
-    }
   }
 
   // Click event
