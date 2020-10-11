0e77ed9fc41a8ae14b6f7819deba46df9f200977
b.swierzewski <swierzewski.bartosz@gmail.com>
2020-10-11 09:07:00 +0200

    Change name in production enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  socketUrl: 'http://localhost:3000/',
-  authUrl: 'http://localhost:5001/api/',
-  backendUrl: 'http://localhost:5002/api/',
-  microcontrollerUrl: 'http://localhost:5003/api/',
+  socketUrl: 'http://micro.io:3000/',
+  authUrl: 'http://micro.io:5001/api/',
+  backendUrl: 'http://micro.io:5002/api/',
+  microcontrollerUrl: 'http://micro.io:5003/api/',
 };
