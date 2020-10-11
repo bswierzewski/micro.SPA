6b534b0c26436be7a9dbbb6f8e80c0db1edf7ea6
b.swierzewski <swierzewski.bartosz@gmail.com>
2020-10-11 21:18:26 +0200

    Install store and effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,8 +7,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { JwtModule } from '@auth0/angular-jwt';
 
+// Services
 import { ErrorInterceptorProvider } from 'src/app/core/interceptors/error.interceptor';
 import { AlertService, AuthService } from './core/services';
+
+// Store
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 export function tokenGetter(): string {
   return localStorage.getItem('token');
@@ -25,6 +33,10 @@ export function tokenGetter(): string {
         tokenGetter,
       },
     }),
+    // Modules
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
   ],
   providers: [ErrorInterceptorProvider, AuthService, AlertService],
   bootstrap: [AppComponent],
