71e7107b3ea6f3f78e3f6d016d9bee267553d2e3
b.swierzewski <b.swierzewski@7technology.pl>
2020-10-16 14:11:17 +0200

    Refactoring device detail

--- src/app/modules/home/device/device-detail/device-detail.component.ts
+++ src/app/modules/home/device/device-detail/device-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Device, Registration } from 'src/app/shared/models';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -8,25 +8,37 @@ import * as fromRoot from '../../../../store/app.reducer';
 import * as DeviceActions from '../../../../store/actions/device.actions';
 import * as RegistrationActions from '../../../../store/actions/registration.actions';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-device-detail',
   templateUrl: './device-detail.component.html',
   styleUrls: ['./device-detail.component.scss'],
 })
-export class DeviceDetailComponent implements OnInit {
+export class DeviceDetailComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['position', 'created', 'address', 'rssi'];
 
   isLoading$: Observable<boolean>;
   device: Device;
-  registrations$: Observable<Registration[]>;
+  dataSource = new MatTableDataSource<Registration>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private route: ActivatedRoute, private store: Store<fromRoot.State>) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.store.dispatch(DeviceActions.loadDevice({ id: Number(params.id) }));
-      this.registrations$ = this.store.select(fromRoot.getRegistrations);
+      this.store
+        .pipe(
+          select(fromRoot.getRegistrations),
+          first((registrations) => registrations !== null)
+        )
+        .subscribe((registrations) => {
+          this.dataSource.data = registrations;
+        });
       this.store
         .pipe(
           select(fromRoot.getDevice),
@@ -37,5 +49,19 @@ export class DeviceDetailComponent implements OnInit {
           this.store.dispatch(RegistrationActions.loadRegistrations({ id: device.id }));
         });
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
