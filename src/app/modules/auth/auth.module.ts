1f69b217c5d3430a614f2a8a4beba1c2d22ddc2e
b.swierzewski <swierzewski.bartosz@gmail.com>
2020-10-19 20:28:09 +0200

    Add registration view

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent],
-  imports: [SharedModule, AuthRoutingModule],
+  imports: [SharedModule, AuthRoutingModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   exports: [],
 })
 export class AuthModule {}
