874da7b4a11b38461527412d7eb3b40d45d09825
b.swierzewski <b.swierzewski@7technology.pl>
2020-10-20 08:15:25 +0200

    Refactoring register component

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -6,6 +6,10 @@ import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: LoginComponent,
+  },
+  {
     path: 'login',
     component: LoginComponent,
   },
