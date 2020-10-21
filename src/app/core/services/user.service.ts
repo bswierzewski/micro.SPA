f048836af8d20d3ef0b7a7f56e6ebfb479e99d5d
b.swierzewski <b.swierzewski@7technology.pl>
2020-10-21 13:05:10 +0200

    Add user service

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,22 @@
+import { User } from 'src/app/shared/models';
+import { environment } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  userUrl = environment.authUrl + 'users';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.userUrl);
+  }
+
+  activateUser(user: User): Observable<any> {
+    return this.httpClient.post(this.userUrl + '/activated', user);
+  }
+}
