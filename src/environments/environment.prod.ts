31e223fdd309ae17a890a4749bcbc6ed689ff556
b.swierzewski <swierzewski.bartosz@gmail.com>
2020-11-19 18:37:34 +0100

    Remove .io

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  socketUrl: 'http://micro.io:3000/',
-  authUrl: 'http://auth.micro.io/api/',
-  backendUrl: 'http://backend.micro.io/api/',
-  microcontrollerUrl: 'http://microcontroller.micro.io/api/',
+  socketUrl: 'http://micro:3000/',
+  authUrl: 'http://auth.micro/api/',
+  backendUrl: 'http://backend.micro/api/',
+  microcontrollerUrl: 'http://microcontroller.micro/api/',
 };
